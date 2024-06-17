19b0759bb10df2a97fb4a247e4dedb68a41b2d5e
Guilherme Akio <guiakio8@gmail.com>
2024-06-17 15:50:52 -0300

    Criando base da classe

--- SistemaLivraria/src/main/java/org/example/ClientDebt.java
+++ SistemaLivraria/src/main/java/org/example/ClientDebt.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class ClientDebt {
+    String client;
+    String book;
+
+    public ClientInDebt(String clientName, String bookTitle) {
+        this.client = clientName;
+        this.book = bookTitle;
+    }
 }
